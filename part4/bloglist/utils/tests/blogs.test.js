21fc11289e3f1ec9963284cda04cbc8b3e137c23
Anas Gamal <agamal@stetson.edu>
2023-06-19 18:49:03 +0300

    add unit test for one blog case

--- part4/bloglist/utils/tests/blogs.test.js
+++ part4/bloglist/utils/tests/blogs.test.js
@@ -50,6 +50,16 @@ describe('Statistics', () => {
       __v: 0
     }  
   ]
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
 
 test('blogs return favorite blog', () => {
   const result = listHelper.favoriteBlog(blogs)
@@ -59,5 +69,9 @@ test('blogs return favorite blog', () => {
 test('blogs return total likes', () => {
   const result = listHelper.totalLikes(blogs)
   expect(result).toBe(36)
+})
+test('when list has only one blog, equals the likes of that', () => {
+  const result = listHelper.totalLikes(listWithOneBlog)
+  expect(result).toBe(5)
 })
 })
