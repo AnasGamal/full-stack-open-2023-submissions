daec6f9aa965a76983bffeb926eefb03399447ad
Anas Gamal <agamal@stetson.edu>
2023-07-16 21:43:39 +0300

    implement validation for username and password

--- part4/notes-backend/controllers/users.js
+++ part4/notes-backend/controllers/users.js
@@ -2,21 +2,34 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+// TODO: implement tests in /test/useres.test.js
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+  if (username && password !== '') {
+    if (username.length < 3) return response.status(400).json({
+      error: 'Username must be at least 3 characters long.'
+    })
+    if (password.length < 3) return response.status(400).json({
+      error: 'Username must be at least 3 characters long.'
+    })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  })
+    const savedUser = await user.save()
 
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+    response.status(201).json(savedUser)
+  } else {
+    return response.status(400).json({
+      error: 'Both username and password must be provided.'
+    })
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
