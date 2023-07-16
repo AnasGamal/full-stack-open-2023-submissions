61d8a23149174ce416566bd32263ba69e80858ab
Anas Gamal <agamal@stetson.edu>
2023-07-16 20:19:28 +0300

    unique validation

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -14,6 +15,18 @@ const userSchema = new mongoose.Schema({
       ref: 'Note'
     }
   ],
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
 })
 
 const User = mongoose.model('User', userSchema)
