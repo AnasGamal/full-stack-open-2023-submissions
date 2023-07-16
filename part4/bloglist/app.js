738730846d7745fb945a9ab0d187d30f890bc502
Anas Gamal <agamal@stetson.edu>
2023-07-16 20:16:00 +0300

    exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,8 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
+
+
 mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -18,6 +21,8 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
