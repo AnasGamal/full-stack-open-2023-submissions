ed71a5371a3c29b46bdeb33b5ec0ff8d7e28a59d
Anas Gamal <agamal@stetson.edu>
2023-07-17 11:32:46 +0300

    implement validation in adding users

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const uniqueValidator = require('mongoose-unique-validator')
 
 usersRouter.get('/', async (request, response) => {
     const users = await User
@@ -10,7 +11,14 @@ usersRouter.get('/', async (request, response) => {
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
-
+  if (username && password !== '') {
+    if (username.length < 3) return response.status(400).json({
+      error: 'Username must be at least 3 characters long.'
+    })
+    if (password.length < 3) return response.status(400).json({
+      error: 'Username must be at least 3 characters long.'
+    })
+  
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
@@ -20,9 +28,23 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   })
 
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+  try {
+    const savedUser = await user.save()
+  
+    response.status(201).json(savedUser)
+    } catch (error) {
+      if (error.name = "ValidationError") {
+      return response.status(409).json({ error: error.message})
+      }
+      else {
+        throw error
+      }
+    }
+  } else {
+    return response.status(400).json({
+      error: 'Both username and password must be provided.'
+    })
+  }
 })
 
 module.exports = usersRouter
