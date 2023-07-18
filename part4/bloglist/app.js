aaddbd0656559993c76c47a4a7f0e31f2cd97b24
Anas Gamal <agamal@stetson.edu>
2023-07-18 16:20:06 +0300

    Add middleware and error handler

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,7 +7,7 @@ const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
@@ -22,10 +22,13 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.get('/', (request, response) => {
     Blog
       .find({}).populate('user', { username: 1, name: 1 })
@@ -11,7 +21,11 @@ blogsRouter.get('/', (request, response) => {
   
 blogsRouter.post('/', async (request, response) => {
     const body = request.body 
-    const user = await User.findOne()
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
     const savedBlog = new Blog ({
       ...body,
       user: user.id
