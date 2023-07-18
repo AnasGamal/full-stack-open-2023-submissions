76b22cd198f778a46ca5e3331ebd5f3344c02ab5
Anas Gamal <agamal@stetson.edu>
2023-07-18 14:39:13 +0300

    complete 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
 blogsRouter.get('/', (request, response) => {
     Blog
       .find({}).populate('user', { username: 1, name: 1 })
@@ -9,14 +9,21 @@ blogsRouter.get('/', (request, response) => {
       })
   })
   
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body 
+    const user = await User.findOne()
+    const savedBlog = new Blog ({
+      ...body,
+      user: user.id
+    })
+    
+    savedBlog
       .save()
       .then(result => {
         response.status(201).json(result)
       })
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
   })
 
 module.exports = blogsRouter  
