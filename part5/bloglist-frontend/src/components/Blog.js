b1c534b0bcdcb487a78bb39360c416b91cf3be41
Anas Gamal <agamal@stetson.edu>
2023-07-27 18:41:42 +0300

    make blog details collapsable

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+
+  const [detailsVisible, setdetailsVisible] = useState(false)
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  const handleViewBlog = () => {
+    setdetailsVisible(!detailsVisible)
+  }
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+  <div style={blogStyle}>
+    {blog.title}
+    <button onClick={handleViewBlog}>
+      {detailsVisible ? "hide" : "view"}
+    </button>
+    
+    <div style={showWhenVisible}>
+    <p>{blog.url}</p>
+    <p>{blog.likes} <button>like</button></p>
+    <p>{blog.author}</p>
+    </div>
+  </div>
+  )
+}
 
 export default Blog
