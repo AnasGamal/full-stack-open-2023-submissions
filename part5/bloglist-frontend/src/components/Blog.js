a00ee2307266231a589c9c7cdf99b78171647ac9
anasgamal <anasgaamal@gmail.com>
2023-07-28 15:28:18 +0300

    implement remove functionality in the ui

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -4,6 +4,7 @@ import blogService from '../services/blogs'
 const Blog = ({blog}) => {
   const [displayedBlog, setDisplayedBlog] = useState(blog)
 
+
   const [detailsVisible, setdetailsVisible] = useState(false)
   const showWhenVisible = { display: detailsVisible ? '' : 'none' }
 
@@ -34,7 +35,25 @@ const Blog = ({blog}) => {
     marginBottom: 5
   }
 
+  const handleRemoveClick = (blog) => {
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+      blogService
+      .remove(blog.id)
+      .then(() => {
+        setDisplayedBlog(null)
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          alert('Unauthorized: Only the creator can delete the blog')
+        }
+      })
+    }
+  }
+  if (displayedBlog === null) {
+    return null
+  }
   return (
+    
   <div style={blogStyle}>
     {blog.title}
     <button onClick={handleViewBlog}>
@@ -45,6 +64,7 @@ const Blog = ({blog}) => {
     <p>{blog.url}</p>
     <p>{displayedBlog.likes} <button onClick={() => handleLikeClick(blog)}>like</button></p>
     <p>{blog.author}</p>
+    <p><button onClick={() => handleRemoveClick(blog)}>remove</button></p>
     </div>
   </div>
   )
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -21,5 +21,12 @@ const update = async (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject)
   return request.then(response => response.data)
 }
+
+const remove = async (id) => {
+  const config = { headers: { Authorization: token } }
+  const request = axios.delete(`${baseUrl}/${id}`, config)
+  return request.then(response => response.data)
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, create, update, setToken }
+export default { getAll, create, remove, update, setToken }
