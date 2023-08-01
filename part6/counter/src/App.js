a2918253dd97141d011c325a750be192aaf4fd12
Anas Gamal <agamal@stetson.edu>
2023-08-02 01:49:16 +0300

    initalize counter

---
+++ part6/counter/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      {count}
+      <div>
+        <button onClick={() => setCount(count + 1)}>plus</button>
+        <button onClick={() => setCount(count - 1)}>minus</button>
+        <button onClick={() => setCount(0)}>zero</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
