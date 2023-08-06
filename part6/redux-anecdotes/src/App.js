befa479a0f27021bbaca9ea43d926697018154c6
Anas Gamal <agamal@stetson.edu>
2023-08-06 11:04:14 +0300

    seperate anecdoteform into its own component

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,34 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, incrementVoteOf } from './reducers/anecdoteReducer';
-// import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
-
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
-
-
-
   return (
     <div>
       <h2>Anecdotes</h2>
       <AnecdoteList />
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote" /></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
